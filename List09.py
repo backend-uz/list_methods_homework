ac11fcb90e198edf98b488c855f4fee6a7f6675f
user1 <backenduz@gmail.com>
2022-12-03 13:33:01 +0500

    09

--- List09.py
+++ List09.py
@@ -8,10 +8,11 @@ def main(fruits):
     """
     i = 0
     b = []
+    
     while i < len(fruits):
         b.append(i)
         i += 1
     return b
-    
+
 mevalar = ['apple','apple','olma']
 print(main(mevalar))
