095339b61c12d7e44a42cd29ebc18587fdfc6dee
user1 <backenduz@gmail.com>
2022-12-03 13:30:46 +0500

    09

--- List09.py
+++ List09.py
@@ -6,4 +6,12 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    b = []
+    while i < len(fruits):
+        b.append(i)
+        i += 1
+    return b
+    
+mevalar = ['apple','apple','olma']
+print(main(mevalar))
